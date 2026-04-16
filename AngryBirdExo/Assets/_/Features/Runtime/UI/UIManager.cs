c2e7dc4288cd77fa4dbde4f991e7ba84a1a791b5
ryoaspi <>
2026-04-16 12:51:24 +0200

    [Dev] ajustement pour BuildWeb

--- AngryBirdExo/Assets/_/Features/Runtime/UI/UIManager.cs
+++ AngryBirdExo/Assets/_/Features/Runtime/UI/UIManager.cs
@@ -5,6 +5,20 @@ namespace Runtime.UI
 {
     public class UIManager : MonoBehaviour
     {
+        private void Start()
+        {
+            
+#if UNITY_WEBGL_API
+
+        _exitButton.SetActive(false);
+
+#endif
+            // if (Application.platform == RuntimePlatform.WebGLPlayer)
+            // {
+            //     _exitButton.SetActive(true);
+            // }
+        }
+
         public void RestartGame()
         {
             Time.timeScale = 1f;
@@ -15,5 +29,7 @@ namespace Runtime.UI
         {
             Application.Quit();
         }
+        
+        [SerializeField] private GameObject _exitButton;
     }
 }
